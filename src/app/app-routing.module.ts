36c5dd3bca376e6e0f76e6e7952a4172accef2dc
Jaime <jaimecaballero99@gmail.com>
2024-04-15 15:24:49 +0200

    Fin sección 13

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-// dominio.com/
+// dominio.com/ --> rutas padre
 const routes: Routes = [
   {
     path: 'auth',
