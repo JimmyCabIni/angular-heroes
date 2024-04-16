0443c5d6dc8ff9841614191cf741432e1caa6797
Jaime <jaimecaballero99@gmail.com>
2024-04-16 14:52:01 +0200

    Creación de Guard (no tiene todavía la funcionalidad)

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
-export class ServiceNameService {
+export class AuthService {
 
   private baseUrl = environments.baseUrl;
   private user?: User;
@@ -24,8 +24,27 @@ export class ServiceNameService {
     return this.http.get<User>(`${ this.baseUrl }/users/1`)
       .pipe(
         tap( user => this.user = user ),
-        tap( user => localStorage.setItem('token', user.id.toString() )),
+        tap( user => localStorage.setItem('token', 'ASsdfasfasf.asdfa.asdfa' )),
       );
+  }
+
+  checkAuthentication(): Observable<boolean> | boolean {
+
+    if ( !localStorage.getItem('token') ) return false;
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false) )
+      );
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
   }
 
 }
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
 
 @Component({
   selector: 'app-layout-page',
@@ -12,5 +16,19 @@ export class LayoutPageComponent {
     { label: 'Añadir', icon: 'add', url: './new-hero'},
     { label: 'Buscar', icon: 'search', url: './search'},
   ]
+
+  get user():User | undefined {
+    return this.authService.currentUser;
+  }
+
+  constructor(
+    private authService: AuthService,
+    private rooter: Router
+  ){}
+
+  onLogout() {
+    this.authService.logout();
+    this.rooter.navigate(['/auth/login'])
+  }
 
 }
